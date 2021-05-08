96bca28866c35ef7f3c64d6a0afa315d08769974
Asraful Islam <iasraful321@gmail.com>
2021-05-08 19:35:01 +0600

    final comment

---
+++ Server/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCh8GJvLbpC4p-QsYIsmFZifH8IA0qDqPM",
+    authDomain: "asraful-s-portfolio.firebaseapp.com",
+    projectId: "asraful-s-portfolio",
+    storageBucket: "asraful-s-portfolio.appspot.com",
+    messagingSenderId: "534797698443",
+    appId: "1:534797698443:web:e11564e15f2405a27b5b8e",
+    measurementId: "G-ELEWFP92D2"
+  };
